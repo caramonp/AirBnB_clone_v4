5c6219da265aa1fc96a011c928f738411d7fa531
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-09-26 15:00:32 -0500

    conditionals for variables

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -31,21 +31,24 @@ function request (){
     contentType: 'application/json',
     success: function (data){
     for (place in data){
+      cond1 = place.max_guest !== 1;
+      cond2 = place.number_rooms !== 1;
+      cond3 = place.number_bathrooms !== 1;
       $('body').append(`<article>
 	  <div class="title_box">
-	    <h2>${{ place.name }}</h2>
-	    <div class="price_by_night">${{ place.price_by_night }}</div>
+	    <h2>${ place.name }</h2>
+	    <div class="price_by_night">${ place.price_by_night }</div>
 	  </div>
 	  <div class="information">
-	    <div class="max_guest">${{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}</div>
-            <div class="number_rooms">${{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
-            <div class="number_bathrooms">${{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
+	    <div class="max_guest">${ place.max_guest } Guest${cond1}s{% endif %}</div>
+            <div class="number_rooms">${ place.number_rooms } Bedroom${cond2}s{% endif %}</div>
+            <div class="number_bathrooms">${ place.number_bathrooms } Bathroom${cond3}s{% endif %}</div>
 	  </div>
 	  <div class="user">
-            <b>Owner:</b> ${{ place.user.first_name }} ${{ place.user.last_name }}
+            <b>Owner:</b> ${ place.user.first_name } ${ place.user.last_name }
           </div>
           <div class="description">
-	    ${{ place.description | safe }}
+	    ${ place.description | safe }
           </div>`);
     }
     }
