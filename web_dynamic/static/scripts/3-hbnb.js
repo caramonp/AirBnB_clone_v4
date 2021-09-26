4080ceaa273ee0874f77da7d58a78d5d8283c20b
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-09-26 14:10:43 -0500

    point 4

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -21,3 +21,33 @@ const url = 'http://192.168.100.28:5001/api/v1/status/';
     }
   });
 });
+
+function request (){
+  $.ajax({
+    url = 'http://0.0.0.0:5001/api/v1/places_search/',
+    type = "POST",
+    data = {},
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data){
+    for (place in data){
+      $('body').append(`<article>
+	  <div class="title_box">
+	    <h2>{{ place.name }}</h2>
+	    <div class="price_by_night">${{ place.price_by_night }}</div>
+	  </div>
+	  <div class="information">
+	    <div class="max_guest">{{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}</div>
+            <div class="number_rooms">{{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
+            <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div>
+	  </div>
+	  <div class="user">
+            <b>Owner:</b> {{ place.user.first_name }} {{ place.user.last_name }}
+          </div>
+          <div class="description">
+	    {{ place.description | safe }}
+          </div>`)
+    }
+    }
+  })
+}
